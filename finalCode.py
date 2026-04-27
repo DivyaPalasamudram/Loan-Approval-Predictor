9575cd552a1b4c95b6350a13c3f40d2e4bf1b2f9
DivyaPalasamudram <divpalas@gmail.com>
2026-04-27 19:07:27 -0400

    Update finalCode.py

--- finalCode.py
+++ finalCode.py
@@ -15,7 +15,7 @@ import sklearn
 # Load the trained model
 # --- Put the Model in Drive First---
 scalar =  pickle.load(open(r"scaler.pkl", "rb"))
-model = pickle.load(open(r"/content/LoanApprovalModel.pkl", "rb"))
+model = pickle.load(open(r"LoanApprovalModel.pkl", "rb"))
 
 # Set the title of the app
 
